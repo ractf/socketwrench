aa40500946ac91275b50989e1fa179d5740d5a32
404dcd <ben.github@thegenius.org.uk>
2021-07-09 15:24:06 +0100

    Documents config better

--- config/config.go
+++ config/config.go
@@ -2,27 +2,23 @@ package config
 
 import "os"
 
-var RedisAddr, RedisSub, HttpPath, WebsocketAddr, BackendAddr string
+var RedisAddr, RedisSub, WebsocketAddr, BackendAddr string
 
 func init() {
 	var ok bool
-	RedisAddr, ok = os.LookupEnv("REDIS_ADDR")
+	RedisAddr, ok = os.LookupEnv("REDIS_ADDR") // where is redis, passed to redis.NewClient
 	if !ok {
 		panic("Missing env var REDIS_ADDR")
 	}
-	RedisSub, ok = os.LookupEnv("REDIS_SUB")
+	RedisSub, ok = os.LookupEnv("REDIS_SUB") // what is the redis pub/sub called
 	if !ok {
 		panic("Missing env var REDIS_SUB")
 	}
-	HttpPath, ok = os.LookupEnv("HTTP_PATH")
-	if !ok {
-		panic("Missing env var HTTP_PATH")
-	}
-	WebsocketAddr, ok = os.LookupEnv("WEBSOCKET_ADDR")
+	WebsocketAddr, ok = os.LookupEnv("WEBSOCKET_ADDR") // where to host the http upgrade handler, passed to http.ListenAndServe
 	if !ok {
 		panic("Missing env var WEBSOCKET_ADDR")
 	}
-	BackendAddr, ok = os.LookupEnv("BACKEND_ADDR")
+	BackendAddr, ok = os.LookupEnv("BACKEND_ADDR") // where is backend, passed to http.NewRequest, should not end in /
 	if !ok {
 		panic("Missing env var BACKEND_ADDR")
 	}
